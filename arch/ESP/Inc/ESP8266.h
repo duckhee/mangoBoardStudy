69d831344d282ff012ce666121e8b50ad500c7b0
duckhee <fain9301@yahoo.com>
2019-03-31 15:47:21 +0900

    tesitngf

--- arch/ESP/Inc/ESP8266.h
+++ arch/ESP/Inc/ESP8266.h
@@ -1,12 +1,22 @@
 #ifndef __ESP8266_H__
 #define __ESP8266_H__
 
+/* include header */
+#include "Hw_Config.h"
+
+
 /*
     PA2 UART2_TXD 16
     PA3 UART2_RXD 17
     using interrupt usart 
     AT+COMMAND 사용시에 공북 없다. 공백 제거 필수
 */
+
+#define ESPUSART USART3
+#define ESPPORT  GPIOB
+
+
+
 
 
 #endif
--- arch/Inc/main.h
+++ arch/Inc/main.h
@@ -1,6 +1,21 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
+/**
+ * include hw_config file 
+ */
+#include "Hw_Config.h"
+
+
+/**
+ * include esp file
+ * http header 
+ */
+
+/**
+ * include freeRTOS include
+*/
+
 
 
 #endif
