3526f2db003cb514e2128691572653469fd03070
duckhee <fain9301@yahoo.com>
2019-04-10 15:08:38 +0900

    testing

--- arch/ESP/Inc/TCP.h
+++ arch/ESP/Inc/TCP.h
@@ -3,4 +3,18 @@
 
 #include "ESP8266.h"
 
+
+
+
+#ifdef TCP_LOCAL
+#define TCP_DEF
+#else
+#define TCP_DEF         extern
+#endif
+
+TCP_DEF bool PingTest(uint8_t *url);
+TCP_DEF bool TCP_Connect(uint8_t *url, uint8_t *port);
+TCP_DEF bool TCP_Send();
+
+
 #endif
--- arch/ESP/Src/TCP.c
+++ arch/ESP/Src/TCP.c
@@ -2,4 +2,39 @@
 
 #include "TCP.h"
 
+TCP_DEF bool PingTest(uint8_t *url);
+TCP_DEF bool TCP_Connect(uint8_t *url, uint8_t *port);
 
+
+TCP_DEF bool PingTest(uint8_t *url)
+{
+    uint8_t temp[128];
+    bzero(temp, sizeof(temp));
+    sprintf(temp, "AT+PING=\"%s\"\r\n", url);
+    ESP_SendCommand(temp);
+    if(esp8266ReadForResponse("OK\r\n", COMMAND_RESPONSE_TIMEOUT))
+    {
+        return TRUE;
+    }else{
+        return FALSE;
+    }
+}
+
+TCP_DEF bool TCP_Connect(uint8_t *url, uint8_t *port)
+{
+    uint8_t temp[128];
+    bool flag = FALSE;;
+    bzero(temp, sizeof(temp));
+    sprintf(temp, "AT+CIPSTART=\"TCP\",\"%s\",\"%d\"\r\n", url, port);
+    ESP_SendCommand(temp);
+    if(esp8266ReadForResponse("OK\r\n", COMMAND_RESPONSE_TIMEOUT))
+    {
+        flag = TRUE;
+    }else{
+        if(esp8266SearchBuffer("ALREADY"))
+        {
+            flag = TRUE;
+        }
+    }
+    return flag;
+}
