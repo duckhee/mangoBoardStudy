6200d07cf3db06982e608c1a5d281b6377e324d6
duckhee <fain9301@yahoo.com>
2019-05-05 01:49:58 +0900

    why did't golbal set const not work explan

--- arch/ESP/Inc/HTTP.h
+++ arch/ESP/Inc/HTTP.h
@@ -10,6 +10,17 @@ typedef enum{
     DELETE = 3
 }HTTP_MethodTypeDef;
 
+/**
+  1) 상수인 경우
+- internal linkage를 가짐(const int가 static const int와 동일한 의미를 가짐)
+- 각기 다른 곳에서 include 할 때 변수의 주소를 출력해보면 다르게 출력됨을 알 수 있음
+
+2) 상수가 아닌 경우
+- external linkage를 가짐
+- 각기 다른 곳에서 incldue 할 때 변수의 주소를 출력해보면 동일 해야만 함.
+- 따라서 헤더파일에는 변수의 선언만 해야함.
+정의를 해버린다면, include 할 때 마다 하나의 변수이름 대응되는 주소가 추가되버림.
+ */
 
 
 typedef struct{
